c87b802bf5e5594484578987552c1ef3266cfbd6
d4rkstat1c <44454186+d4rk007@users.noreply.github.com>
2019-06-20 01:04:17 +0000

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@
 apt-get isntall net-tools
 apt-get install whois
 apt-get install nmap
-sudo apt-get install dialog
+apt-get install dialog
 mv .blueghost $HOME/
 echo 'source ~/.blueghost' >> ~/.bashrc
 source ~/.bashrc
